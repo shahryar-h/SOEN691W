92cca16240e6d9855003adce304a03cb07906fde
mitraazari <32073496+mitraazari@users.noreply.github.com>
2019-05-05 11:24:16 -0400

    changeMetricsRefactoring

--- changeMetrics.js
+++ changeMetrics.js
@@ -13,7 +13,7 @@ require.extensions['.txt'] = function (module, filename) {
 
 var changes = require("./test.txt").toString();
 changes = changes.trim();
-// changes  = changes.replace(/          [0-9].*/,"");
+
 changes = changes.split(/\n/);
 
 for (var i = 0; i < changes.length; i++) {
@@ -23,19 +23,19 @@ for (var i = 0; i < changes.length; i++) {
 
 for (var i = 0; i < changes.length; i++) {
   changes[i] = changes[i].replace(/(\.\.)(.*)( )/g,"");
-  //console.log(changes[i]);
+  
 }
 for (var i = 0; i < changes.length; i++) {
   if (  /\.java/.test(changes[i])){
     var obj = /([^\/]+)(\.java)/.exec(changes[i]);
     var obj2 = /[0-9]+/.exec(changes[i]);
       if(obj !=null ){
-        //console.log(obj[0]);
+        
       var filename = obj[0];
       filenames.push(filename);
 }
 if(obj2 !=null ){
-        //console.log(obj[0]);
+        
       var timesChanged = obj2[0];
       filenames.push(timesChanged);
 }
@@ -57,12 +57,4 @@ fs.writeFile("changeMetrics.csv", result, function(err) {
 
     console.log("The file was saved!");
 });
-// fs.writeFile("test2.txt", changes, function(err) {
-//     if(err) {
-//         return console.log(err);
-//     }
-//
-//     console.log("The file was saved!");
-// });
 
-//console.log(changes);
